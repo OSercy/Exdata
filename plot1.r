ce3393cdfa2004c9d67449ec274e1aa706aead88
OSercy <odile.sercy@gmail.com>
2014-06-09 09:21:35 +0200

    Update plot1.r

--- plot1.r
+++ plot1.r
@@ -1,5 +1,9 @@
-data=read.table("power.txt",header=TRUE,sep=";",na.strings="?")
+data_full <- read.csv("power.txt",header=TRUE,sep=";",na.strings="?", check.names=F, stringsAsFactors=F, comment.char="",quote='\"')
+data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
 png(file="plot1.png")
-hist(data$Global_active_power, col="red")
-title(main = "Global Active Power", sub = NULL, xlab = "Global Active Power", ylab = "Frequency")
+hist(data$Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 dev.off()
+
